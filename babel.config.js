b616d5871bcd7761db06f5ba31e56520800b20a9
goooon <goooon.development@gmail.com>
2023-12-05 13:48:50 +0900

    テスト導入

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // ES6+ の機能をトランスパイル
+    '@babel/preset-react', // JSX をトランスパイル
+    '@babel/preset-typescript', // TypeScript をトランスパイル
+  ],
+};
+  
---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!string-width|cliui)/'
+  ],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+};
