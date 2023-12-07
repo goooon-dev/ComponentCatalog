5f5e2edf2996a48ebef8b112c8326e0f1133b807
goooon <goooon.development@gmail.com>
2023-12-08 01:12:05 +0900

    update story

--- src/stories/atoms/user-profile-icon/UserProfileIcon.stories.tsx
+++ src/stories/atoms/user-profile-icon/UserProfileIcon.stories.tsx
@@ -1,11 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
 import { UserProfileIcon } from './UserProfileIcon';
 
-export default {
-  title: 'Components/UserProfileIcon',
+const meta: Meta = {
+  title: 'Atoms/UserProfileIcon',
+  tags: ['autodocs'],
   component: UserProfileIcon,
+  argTypes: {
+    imageUrl: { control: 'text' },
+    altText: { control: 'text' },
+    size: { control: 'text' },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    altText: 'ユーザー名',
+  },
 };
 
-export const Default = () => <UserProfileIcon altText="ユーザー名" />;
-export const DefaultLargeSize = () => <UserProfileIcon altText="ユーザー名" size="100px" />;
-export const WithImage = () => <UserProfileIcon imageUrl="src/assets/react.svg" altText="ユーザー名" />;
-export const LargeSize = () => <UserProfileIcon imageUrl="src/assets/react.svg" altText="ユーザー名" size="100px" />;
+export const DefaultLargeSize: Story = {
+  args: {
+    altText: 'ユーザー名',
+    size: '100px',
+  },
+};
+
+export const WithImage: Story = {
+  args: {
+    imageUrl: 'src/assets/react.svg',
+    altText: 'ユーザー名',
+  },
+};
+
+export const LargeSize: Story = {
+  args: {
+    imageUrl: 'src/assets/react.svg',
+    altText: 'ユーザー名',
+    size: '100px',
+  },
+};
